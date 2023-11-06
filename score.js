102321ddc3f0ea9cc78b53e5da38b846d646af66
Parthsss <parth.shah@kalvium.community>
2023-11-06 17:15:31 +0530

    added js and functionality

--- score.js
+++ score.js
@@ -1,1 +1,16 @@
+
 // Iteration 8: Making scoreboard functional
+
+var urlParams = new URLSearchParams(window.location.search);
+var score = urlParams.get("score");
+
+
+var scoreBoard = document.getElementById("score-board");
+
+var playAgaininButton = document.getElementById("play-again-button")
+
+scoreBoard.innerHTML = score;
+
+playAgaininButton.onclick = () => {
+    location.href = "./game.html";
+}
